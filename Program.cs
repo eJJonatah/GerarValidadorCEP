484a0214f8735c1bc2143b6e28cfda5cd687f5b0
James <eJJonatah>
2022-12-01 10:58:15 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine(GerarCEPVal());
+        }
+    }
+    public static string GerarCEPVal()
+    {
+        System.Threading.Thread.Sleep(300);
+        Random randNum = new();
+        string firstNumber;
+        string secondNumber;
+        string validationRequest;
+        for (; ; )
+        {
+            firstNumber = string.Concat(randNum.Next(1, 9),
+                                        randNum.Next(1, 6),
+                                        randNum.Next(1, 6),
+                                        randNum.Next(1, 5),
+                                        randNum.Next(0, 0));
+
+            secondNumber = "0" + randNum.Next(1,9) + "0";
+            validationRequest = string.Concat("https://viacep.com.br/ws/", firstNumber, secondNumber, "/json/");
+            try
+            {
+                using (Task<string> sendRequest = new HttpClient().GetStringAsync(validationRequest))
+                {
+                    sendRequest.Wait();
+                    if (!sendRequest.Result.Contains("\"erro\""))
+                    {
+                        return firstNumber + "-" + secondNumber;
+                    }
+                }
+            }
+            catch
+            {
+                continue;
+            }
+            System.Threading.Thread.Sleep(300);
+        }
+    }
+}
